b5585741f0c7f91c8f8ff56a27fc89399d095493
Orlando Caesar Donesa <orlandocaesardonesa@gmail.com>
2026-02-11 14:20:49 +0800

    Added test email page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Temporary test route for email verification
+Route::get('/test-email', function () {
+    $testEmail = 'qalertgpt@gmail.com'; // Change this to your test email address
+    
+    Mail::raw('This is a test email from QAlert.', function ($message) use ($testEmail) {
+        $message->to($testEmail)
+                ->subject('QAlert Test Email');
+    });
+
+    return 'Test email sent';
 });
